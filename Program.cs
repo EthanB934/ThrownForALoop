10d439b004522a4f416046b4c6b51466164212f2
Ethan Brown <eb934053@gmail.com>
2025-05-20 12:23:32 -0500

    Learning strings

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Variables are declared by their type
+string theMeaningOfLife = "forty-two";
+
+// The @ is used to create multi-line strings called verbatim strings
+string greeting = @"Welcome to Thrown for a Loop!
+Your one-stop shop for used sporting equipment";
+
+// ;'s are necessary at the end of lines as if complete thoughts
+Console.WriteLine(greeting);
+
+Console.WriteLine("Please, choose an option: ");
+
+Console.WriteLine(theMeaningOfLife);
+
+// This method receives user-input in the console and stores it in the variable
+string response = Console.ReadLine();
+
+// $ allows for string interpolation 
+Console.WriteLine($@"You chose: {response}
+Thank you for your input!");
