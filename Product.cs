b81495c1e392e58b310a04db3d037b08396134d1
Ethan Brown <eb934053@gmail.com>
2025-05-27 16:41:25 -0500

    Created custom type

---
+++ Product.cs
@@ -0,0 +1,7 @@
+// This Product class is a template for building an object
+public class Product
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+    public bool Sold { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Variables are declared by their type
+using System.Reflection.Metadata;
+
 string theMeaningOfLife = "forty-two";
 
 // The @ is used to create multi-line strings called verbatim strings
@@ -22,32 +24,76 @@ List<int> years = new List<int>()
 };
 
 // Initializes a new instance of a list of strings (simplified)
-List<string> sportsGear =
-[
-    "Football",
-    "Hockey Stick",
-    "Boomerang",
-    "Frisbee",
-    "Golf Putter"
-];
+// List<string> sportsGear =
+// [
+//     "Football",
+//     "Hockey Stick",
+//     "Boomerang",
+//     "Frisbee",
+//     "Golf Putter"
+// ];
+
+List<Product> sportsGear = new List<Product>()
+{
+    new Product()
+    {
+        Name = "Football",
+        Price = 15,
+        Sold = false,
+    },
+    new Product()
+    {
+        Name = "Hockey Stick",
+        Price = 25,
+        Sold = true,
+    },
+    new Product()
+    {
+        Name = "Boomerang",
+        Price = 19,
+        Sold = false,
+    },
+    new Product()
+    {
+        Name = "Frisbee",
+        Price = 2,
+        Sold = true,
+    },
+    new Product()
+    {
+        Name = "Golf Putter",
+        Price = 50,
+        Sold = true,
+    }
+};
 
 Console.WriteLine("Please, choose an option: ");
 
+int counter = 0;
+
 for (int i = 0; i < sportsGear.Count; i++)
 {
-    Console.WriteLine($"{i + 1}. {sportsGear[i]}");
+    if (sportsGear[i].Sold == true)
+    {
+        continue;
+    }
+    else
+    {
+        counter++;
+        Console.WriteLine($"{counter}. ${sportsGear[i].Price} {sportsGear[i].Name}");
+    }
 }
 
 // This method receives user-input in the console and stores it in the variable
 int response = int.Parse(Console.ReadLine().Trim());
 
 // A while loop's function body will execute only when the parameter evaluates to true
-while (response > 5 || response < 1)
+while (response > counter || response < counter)
 // IsNullOrEmpty does not account for whitespace
 {
-    Console.WriteLine("Please, choose a number between 1 and 5");
+    Console.WriteLine($"Please, choose a number between 1 and {counter}");
     response = int.Parse(Console.ReadLine().Trim());
 }
 
-Console.WriteLine($@"You chose: {sportsGear[response - 1]}
+Console.WriteLine($@"You chose: {sportsGear[response].Name}
 Thank you for your input!");
